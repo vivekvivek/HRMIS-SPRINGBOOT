ebf47be024bebd4a23f0296cd3881861485c5cf3
Your Name <you@example.com>
2017-12-13 00:21:52 +0530

    updated project name

--- src/main/java/com/javarnd/hrmis/HrmisApplication.java
+++ src/main/java/com/javarnd/hrmis/HrmisApplication.java
@@ -1,4 +1,4 @@
-package org.EnquiryManagementSystem;
+package com.javarnd.hrmis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
