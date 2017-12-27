771d149cddca80a4cef3ca4c30afcc8ca0a7e7f2
Sumit <sumit.javarnd@gmail.com>
2017-12-27 21:41:30 +0530

    removed security changes

--- src/main/java/com/javarnd/hrmis/HrmisApplication.java
+++ src/main/java/com/javarnd/hrmis/HrmisApplication.java
@@ -1,38 +1,17 @@
 package com.javarnd.hrmis;
 
-import java.util.Arrays;
-import java.util.HashSet;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.javarnd.hrmis.model.Authority;
-import com.javarnd.hrmis.model.AuthorityRepository;
 import com.javarnd.hrmis.model.Employee;
-import com.javarnd.hrmis.model.User;
-import com.javarnd.hrmis.model.UserRepository;
 import com.javarnd.hrmis.repository.EmployeeRepository;
 
 @SpringBootApplication
 public class HrmisApplication implements CommandLineRunner {
 
 	private EmployeeRepository employeeRepository;
-	
-	private UserRepository userRepository;
-	
-	private AuthorityRepository authorityRepository;
-	
-	@Autowired
-	public void userRepository(UserRepository userRepository) {
-		this.userRepository = userRepository;
-	}
-	
-	@Autowired
-	public void authorityRepository(AuthorityRepository authorityRepository) {
-		this.authorityRepository = authorityRepository;
-	}
 	
 	@Autowired
 	public void employeeRepository(EmployeeRepository employeeRepository) {
@@ -49,39 +28,6 @@ public class HrmisApplication implements CommandLineRunner {
 			Employee employee = new Employee();
 			employee.setEmpName("Employee 1");
 			employeeRepository.save(employee);
-		}
-		Authority authorityUser = new Authority();
-		authorityUser.setName("USER");
-		Authority authorityAdmin = new Authority();
-		authorityAdmin.setName("ADMIN");
-		if(authorityRepository.count()==0) {
-			authorityRepository.save(authorityUser);
-			authorityRepository.save(authorityAdmin);
-		}
-		if(userRepository.count()==0){
-			User user = new User();
-			user.setUsername("user");
-			user.setEmail("user@mail.me");
-			user.setPassword("userpassword");
-			user.setAuthorities(new HashSet<Authority>(Arrays.asList(authorityUser)));
-			user.setActivated(true);
-			userRepository.save(user);
-			
-			User admin = new User();
-			admin.setUsername("admin");
-			admin.setEmail("admin@mail.me");
-			admin.setPassword("adminpassword");
-			admin.setAuthorities(new HashSet<Authority>(Arrays.asList(authorityAdmin)));
-			admin.setActivated(true);
-			userRepository.save(admin);
-			
-			User superUser = new User();
-			superUser.setUsername("super");
-			superUser.setEmail("super@mail.me");
-			superUser.setPassword("superpassword");
-			superUser.setAuthorities(new HashSet<Authority>(Arrays.asList(authorityUser, authorityAdmin)));
-			superUser.setActivated(true);
-			userRepository.save(superUser);
 		}
 		
 	}
--- src/main/java/com/javarnd/hrmis/model/AuthorityRepository.java
+++
@@ -1,7 +0,0 @@
-package com.javarnd.hrmis.model;
-
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, String> {
-}
